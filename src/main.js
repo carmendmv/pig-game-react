1fd3f8341dee29929e9f3d8f2eb3923fd3ee1cc0
Carmen <dmv.carmencit@gmail.com>
2025-02-10 14:29:51 +0100

    declaracion de variables

--- src/main.js
+++ src/main.js
@@ -25,3 +25,48 @@ document.querySelector("#app").innerHTML = `
       <button class="btn btn--hold">📥 Hold</button>
     </main>
 `;
+
+//1) Seleccionar todos los elementos de DOM:
+
+//activePlayer --> variables de estado en JS
+const sectionPlayer0 = document.querySelector(".player--0");
+const sectionPlayer1 = document.querySelector(".player--1");
+//score = [0, 0] --> variables de estado en JS
+const score0 = document.getElementById("score--0");
+const score1 = document.getElementById("score--1");
+//current --> variables de estado en JS
+const currentScore0 = document.getElementById("current--0");
+const currentScore1 = document.getElementById("current--1");
+
+//botones --> variables del DOM
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+
+//imagen --> variables del DOM
+const dice = document.querySelector(".dice");
+
+//para las variables de estado
+let score, currentScore, activePlayer;
+
+//iniciar los valores a 0 al comenzar la partida
+const initData = () => {
+  //init state variables
+  score = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  //update DOM
+  score0.value = 0;
+  score1.value = 0;
+  currentScore0.value = 0;
+  currentScore1.value = 0;
+
+  sectionPlayer0.classList.remove("player--winner");
+  sectionPlayer1.classList.remove("player--winner");
+  sectionPlayer0.classList.add("player--active");
+  sectionPlayer1.classList.remove("player--active");
+
+  dice.classList.add("hidden");
+};
+
+initData();
